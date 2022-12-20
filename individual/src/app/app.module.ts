53ca13ed0b85128d529f5d9df583aa9c0161969e
ringtail003 <ringtail003@ringtail003-macstudio.local>
2022-12-21 07:46:53 +0900

    bell-icon追加

--- individual/src/app/app.module.ts
+++ individual/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { BellIconComponent } from './bell-icon/bell-icon.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BellIconComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
---
+++ individual/src/app/bell-icon/bell-icon.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-icon-bell-icon',
+  templateUrl: './bell-icon.component.html',
+  styleUrls: ['./bell-icon.component.scss'],
+})
+export class BellIconComponent {}

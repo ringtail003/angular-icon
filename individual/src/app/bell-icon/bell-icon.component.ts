274d8656e96418c0002e4487f3244026a784208d
ringtail003 <ringtail003@ringtail003-macstudio.local>
2022-12-21 08:04:39 +0900

    individualのアイコンを増殖

--- individual/src/app/bell-icon/bell-icon.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'angular-icon-bell-icon',
-  templateUrl: './bell-icon.component.html',
-  styleUrls: ['./bell-icon.component.scss'],
-})
-export class BellIconComponent {}
